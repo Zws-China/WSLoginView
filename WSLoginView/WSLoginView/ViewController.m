31a3c927279ed843701404d2d9c9443d631ccfbc
784846767@qq.com <784846767@qq.com>
2016-12-26 10:35:42 +0800

    .

--- WSLoginView/WSLoginView/ViewController.m
+++ WSLoginView/WSLoginView/ViewController.m
@@ -29,6 +29,8 @@
     [wsLoginV setClickBlock:^(NSString *textField1Text, NSString *textField2Text) {
         
         NSLog(@"点击了登录按钮textField1 = %@   textField2 = %@",textField1Text,textField2Text);
+        UIAlertView *alertV = [[UIAlertView alloc]initWithTitle:@"登录按钮事件" message:[NSString stringWithFormat:@"账号：%@\n密码：%@",textField1Text,textField2Text] delegate:nil cancelButtonTitle:@"确定" otherButtonTitles:nil, nil];
+        [alertV show];
     }];
     
     
